2f151efe3b091cb4c80b4f1cf6afec014a1e7853
aa339519589-cpu <aa339519589@gmail.com>
2026-06-17 19:23:37 +0800

    Load compact home and bottom nav overrides

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './PredictionApp.tsx'
+import './compact-home-bottom-nav.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
